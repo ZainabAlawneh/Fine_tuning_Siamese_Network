0346edfa4657079c6cbd0fedb17d4447c0215888
Zainab Alawneh <Zainab.Alawneh@marsrobotic.com>
2026-03-04 15:17:40 +0300

    adding jubeter notebook

--- fine_tuning.py
+++ fine_tuning.py
@@ -6,6 +6,7 @@ from modules.signature_verification.preprocess import preprocess_signature
 import cv2
 import pandas as pd 
 import torch.nn as nn
+import torch.nn.functional as F
 class ContrastiveLoss(nn.Module):
     def __init__(self, margin=1.0, alpha=1.0, beta=1.0):
         super(ContrastiveLoss, self).__init__()
@@ -42,61 +43,86 @@ class Signnature_dataset(Dataset):
         label = torch.tensor(label, dtype=torch.float32)
         return img1, img2, label
 
-model_path = Path(__file__).parent / "assets" / "models"/ "fine_tunining.pth"
+
+from sklearn.metrics import roc_curve
+import numpy as np
+
+def train(epochs=5):
+
+    model_path = Path(__file__).parent / "assets" / "models"/ "signet.pth"
 
 
-model, _  = load_signet_model(model_path)
+    model, _  = load_signet_model(model_path)
 
-print("Model is loaded successfully")
+    print("Model is loaded successfully")
 
-for params in model.conv_layers.parameters():
-    params.requires_grad = False
+    for params in model.conv_layers.parameters():
+        params.requires_grad = False
 
-# Suppose conv_layers has 5 blocks
-for param in model.conv_layers[-2:].parameters():  # unfreeze last 2 conv blocks
-    param.requires_grad = True
-for name, param in model.named_parameters():
-    print(name, param.requires_grad)
 
-optimizer = torch.optim.Adam(
-    filter(lambda p:p.requires_grad, model.parameters()),
-    lr=1e-5
-)
+    for param in model.conv_layers[-2:].parameters():  # unfreeze last 2 conv blocks
+        param.requires_grad = True
 
-model.train()
 
-data = pd.read_csv("dataset_.csv")
-batch_size = 16
-device = "cuda" if torch.cuda.is_available else "cpu"
-dataset = Signnature_dataset(data.values)
+    optimizer = torch.optim.Adam(
+        filter(lambda p:p.requires_grad, model.parameters()),
+        lr=1e-5
+    )
 
-dataloader = DataLoader(dataset, batch_size, shuffle=True)
-criterion = ContrastiveLoss(margin=1.0)
+    model.train()
 
-epochs = 30
+    data = pd.read_csv("dataset_.csv")
+    batch_size = 16
+    device = "cuda" if torch.cuda.is_available else "cpu"
+    dataset = Signnature_dataset(data.values)
 
-print(f"started on device: {device}")
+    dataloader = DataLoader(dataset, batch_size, shuffle=True)
+    criterion = ContrastiveLoss(margin=1.0)
 
-for epoch in range(epochs) :
-    running_loss = 0.0
+    thresold = 0.2
+    print(f"started on device: {device}")
 
-    for batch_idx, (img1, img2, label )in enumerate(dataloader):
-        img1 = img1.to(device)
-        img2 = img2.to(device)
-        label = label.to(device)
-        optimizer.zero_grad()
-        emb1 = model(img1)
-        emb2 = model(img2)
-        # print(f"emb2 : {emb1}")
-        # print(f"emb2 : {emb2}")
+    for epoch in range(epochs) :
+        running_loss = 0.0
+        
+        all_distances = []
+        all_labels = []
 
-        loss = criterion(emb1, emb2, label)
+        for batch_idx, (img1, img2, label )in enumerate(dataloader):
+            img1 = img1.to(device)
+            img2 = img2.to(device)
+            label = label.to(device)
+            optimizer.zero_grad()
+            emb1 = model(img1)
+            emb2 = model(img2)
+            # print(f"emb2 : {emb1}")
+            # print(f"emb2 : {emb2}")
 
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        print(f"Epoch [{epoch+1}/{epochs}], Batch [{batch_idx+1}/{len(dataloader)}], "
-              f"Loss: {loss.item():.4f}", end='\r')
+            emb1 = F.normalize(emb1, p=2, dim=1)
+            emb2 = F.normalize(emb2, p=2, dim=1)
 
-    print(f"Epoch [{epoch+1}/{epochs}],Loss: {running_loss/len(dataloader):.4f}                      ")
-torch.save(model.state_dict(), "fine_tunining2.pth") 
+            loss = criterion(emb1, emb2, label)
+
+            loss.backward()
+            optimizer.step()
+
+            distance = torch.nn.functional.pairwise_distance(emb1, emb2)
+            
+            all_distances.append(distance.cpu())
+            all_labels.append(label.cpu())
+
+
+            print(f"Epoch [{epoch+1}/{epochs}], Batch [{batch_idx+1}/{len(dataloader)}]", end='\r')
+
+
+        all_distances = torch.cat(all_distances).detach().cpu().numpy()
+        all_labels = torch.cat(all_labels).detach().cpu().numpy()
+        labels_flipped = 1 - all_labels 
+        fpr, tpr, thresholds = roc_curve(labels_flipped, -all_distances)
+
+        fnr = 1 - tpr
+        eer_threshold = thresholds[np.nanargmin(np.absolute((fnr-fpr)))]
+        eer = fpr[np.nanargmin(np.absolute((fnr - fpr)))]
+        print(f"Epoch [{epoch+1}/{epochs}], EER: {eer*100:2f}%, threshold: {eer_threshold:.4f}")
+
+    torch.save(model.state_dict(), "fine_tunining2.pth") 
